2baec6c0eae05c82a30600436d6638df2cfb0c9d
Adrian Mallqui <amallqui38@gmail.com>
2026-03-03 10:47:22 -0500

    Agregar nueva funcion

--- file.py
+++ file.py
@@ -3,4 +3,8 @@ print("Hola mundo")
 
 print("Este es un text para la rama/ejemplo")
 
-# Este comendario solo en ejemplo plis
+
+def clase_prueba() -> int:
+    print("Esta funcion no hace nada pero parece que si")
+    valor = 1
+    return valor
