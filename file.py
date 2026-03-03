b8b3a62ac2dee4f9f60c0e00f1770ea265706355
Adrian Mallqui <amallqui38@gmail.com>
2026-03-03 10:57:55 -0500

    update file.py

--- file.py
+++ file.py
@@ -1,11 +1,4 @@
-print("Hola mundo")
+print("Hola Mundo")
 
 
 print("Este es un text para la rama/ejemplo")
-<<<<<<< HEAD
-=======
-
-# Este comendario solo en ejemplo plis
-
-print("Este es un cambio en GitHub")
->>>>>>> 22d03d0f7e87a8da1ae89bede395b2997254b70b
