29c59ec1a6615b5b6dedbd140ffb947b962d44cb
Adrian Mallqui <amallqui38@gmail.com>
2026-03-03 10:50:44 -0500

    Eliminar Funcion innecesaria

--- file.py
+++ file.py
@@ -2,9 +2,3 @@ print("Hola mundo")
 
 
 print("Este es un text para la rama/ejemplo")
-
-
-def clase_prueba() -> int:
-    print("Esta funcion no hace nada pero parece que si")
-    valor = 1
-    return valor
